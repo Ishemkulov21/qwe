476e8080d8a1069a784e29d086b268147c2f4f4d
Bahram <ishemkulov21@gmail.com>
2022-03-15 21:07:17 +0600

    first commit

---
+++ 3.py
@@ -0,0 +1,11 @@
+proccesor ='AMD Ryzen 5 4600H'
+monitor ='IPS,15,6 Full-HD(1920x1080)'
+disks = '1ТБ HDD + Full-HD'
+chipset = 'AMD'
+video_card ='NVDIA GeForce GTX 1660 Ti' 
+
+print(proccesor)
+print(monitor)
+print(disks)
+print(chipset)
+print(video_card)
